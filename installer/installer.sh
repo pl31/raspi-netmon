ab7f071ff0e06a8e17251cd3b332edb8f9950c72
Patrick Büch <git@paco.pl31.de>
2016-06-08 14:15:30 +0200

    Add iproute2

--- installer/installer.sh
+++ installer/installer.sh
@@ -3,7 +3,7 @@
 echo "Running netmon installer"
 
 echo "Install required packages"
-tce-load -wi git python py-smbus
+tce-load -wi git iproute2 python py-smbus
 
 echo "Clone or update repository to home folder"
 git clone https://github.com/pl31/raspi-netmon.git ~/raspi-netmon || (cd ~/raspi-netmon && git pull)
