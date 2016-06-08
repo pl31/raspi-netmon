13f114dd9bc990b83ec45d881ac4761311c005f4
pl31 <patrick.kowalzick@web.de>
2016-06-08 13:59:36 +0200

    Update installer.sh

--- installer/installer.sh
+++ installer/installer.sh
@@ -5,5 +5,5 @@ echo "Running netmon installer"
 echo "Install required packages"
 tce-load -wi git python py-smbus
 
-echo "Clone repository to home folder"
-git clone https://github.com/pl31/raspi-netmon.git ~/raspi-netmon
+echo "Clone or update repository to home folder"
+git clone https://github.com/pl31/raspi-netmon.git ~/raspi-netmon || (cd ~/raspi-netmon || git pull)
