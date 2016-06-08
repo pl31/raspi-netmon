19f23d61c17bd9e88de2d9eb93f0e91d9b81a520
pl31 <patrick.kowalzick@web.de>
2016-06-08 21:13:28 +0200

    Update start.sh

--- netmon/start.sh
+++ netmon/start.sh
@@ -9,12 +9,6 @@ echo "Running $1" > $LOGFILE
 echo "Set eth0 to promiscious mode" >> $LOGFILE
 ifconfig eth0 promisc
 
-# remove running instances and start netmon async
-echo "Killing any old processes..." >> $LOGFILE
-pgrep -f netmon.py && pkill -f netmon.py
-echo "Starting netmon..." >> $LOGFILE
-( $DIR/netmon.py &> /tmp/netmon.log ) &
-
 echo "Starting lighttpd"
 mkdir -p /tmp/tcpdumps/www
 /usr/local/etc/init.d/lighttpd start
@@ -22,5 +16,11 @@ mkdir -p /tmp/tcpdumps/www
 for i in `seq 0 3`; do ln -s /tmp/tcpdumps/tcpdump_eth0_$i /tmp/tcpdumps/www/tcpdump_eth0_$i.pcap; done
 echo "Start tcpdump"
 tcpdump -n -U -s 0 -i eth0 -W 4 -C 32M -w /tmp/tcpdumps/tcpdump_eth0_ "not ether host $(cat /sys/class/net/eth0/address)" &
+
+# remove running instances and start netmon async
+echo "Killing any old processes..." >> $LOGFILE
+pgrep -f netmon.py && pkill -f netmon.py
+echo "Starting netmon..." >> $LOGFILE
+( $DIR/netmon.py &> /tmp/netmon.log ) &
 
 echo "Start sequence finished" >> $LOGFILE
