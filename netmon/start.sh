1b0bd2042cc77bea3b9d362a0b08885e26d40d49
Patrick Büch <git@paco.pl31.de>
2016-06-08 14:58:16 +0200

    Kill process only if exists

--- netmon/start.sh
+++ netmon/start.sh
@@ -7,8 +7,8 @@ echo "Running $1" > $LOGFILE
 
 # remove running instances and start netmon async
 echo "Killing any old processes..." >> $LOGFILE
-pkill -f netmon.py
-echo "Starting netmon" >> $LOGFILE
+pgrep -f netmon.py && pkill -f netmon.py
+echo "Starting netmon..." >> $LOGFILE
 ~/raspi-netmon/netmon/netmon.py &
 
 echo "Start sequence finished" >> $LOGFILE
