c8962aa03e3859e6ae14583283064ac7b858a524
Patrick Büch <git@paco.pl31.de>
2016-06-08 14:38:43 +0200

    Add start to bootlocal

--- installer/installer.sh
+++ installer/installer.sh
@@ -7,3 +7,7 @@ tce-load -wi git iproute2 python py-smbus
 
 echo "Clone or update repository to home folder"
 git clone https://github.com/pl31/raspi-netmon.git ~/raspi-netmon || (cd ~/raspi-netmon && git pull)
+
+echo "Add start command to /opt/bootlocal.sh"
+BOOTLOCAL="/opt/bootlocal.sh"
+grep -q "# c0f40bf8" $BOOTLOCAL || echo '/home/tc/raspi-netmon/netmon/start.sh	# c0f40bf8' >> $BOOTLOCAL
