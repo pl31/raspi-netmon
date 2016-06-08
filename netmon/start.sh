4eb9e88f5d64e601f9dda6d4560f96e9a0b26b01
Patrick Büch <git@paco.pl31.de>
2016-06-08 14:53:33 +0200

    Add logging

--- netmon/start.sh
+++ netmon/start.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env sh
 
+set -e
+
+LOGFILE=/tmp/start_netmon.log
+echo "Running $1" > $LOGFILE
 
 # remove running instances and start netmon async
+echo "Killing any old processes..." >> $LOGFILE
 pkill -f netmon.py
+echo "Starting netmon" >> $LOGFILE
 ~/raspi-netmon/netmon/netmon.py &
+
+echo "Start sequence finished" >> $LOGFILE
