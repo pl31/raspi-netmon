bb984687aa97124ffefd38d413d7bd53d3419c5d
Patrick Büch <git@paco.pl31.de>
2016-06-10 17:04:16 +0200

    Use package instead of embedded file

--- installer/installer.sh
+++ installer/installer.sh
@@ -9,6 +9,14 @@ echo "---> Freshly clone repository to home folder"
 rm -rf /home/tc/raspi-netmon/
 git clone --depth=1 https://github.com/pl31/raspi-netmon.git /home/tc/raspi-netmon/
 
+echo "---> Install missing modules"
+# clone
+git clone --depth=1 https://github.com/pl31/python-liquidcrystal_i2c.git
+# compile to egg
+(cd /home/tc/python-liquidcrystal_i2c && python setup.py bdist_egg)
+# copy to netmon script
+cp /home/tc/python-liquidcrystal_i2c/dist/*.egg /home/tc/raspi-netmon/netmon
+
 echo "---> Add start command to /opt/bootlocal.sh"
 BOOTLOCAL="/opt/bootlocal.sh"
 grep -q "# c0f40bf8" $BOOTLOCAL || echo '/home/tc/raspi-netmon/netmon/start.sh > /tmp/start_netmon.log	# c0f40bf8' >> $BOOTLOCAL
