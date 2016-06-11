01e5bd8fcefc3f411bd429e91c7b9a44e0847e0c
Patrick Büch <git@paco.pl31.de>
2016-06-11 15:52:43 +0200

    Tidy logging

--- netmon/start.sh
+++ netmon/start.sh
@@ -3,22 +3,22 @@ DIR=`dirname "$0"`
 
 set -e
 
-echo "Running $1"
+echo "---> Running $1"
 
-echo "Set eth0 to promiscious mode"
+echo "---> Set eth0 to promiscious mode"
 ifconfig eth0 promisc
 
-echo "Starting lighttpd"
+echo "---> Starting lighttpd"
 rm -f /var/www/htdocs/index.html
 sed -i '/^dir-listing.activate/c\dir-listing.activate = "enable"' /usr/local/etc/lighttpd/conf.d/dirlisting.conf
 /usr/local/etc/init.d/lighttpd start
 # add symlinks for dumps (add pcap extension)
 for i in `seq 0 3`; do ln -s /tmp/tcpdumps/tcpdump_eth0_$i /var/www/htdocs/tcpdump_eth0_$i.pcap; done
-echo "Start tcpdump"
+echo "---> Start tcpdump"
 mkdir -p /tmp/tcpdumps
 tcpdump -n -U -s 0 -i eth0 -W 4 -C 32M -w /tmp/tcpdumps/tcpdump_eth0_ "not ether host $(cat /sys/class/net/eth0/address)" &
 
-# create i2c groups
+echo "---> Create i2c group"
 addgroup i2c
 chown root:i2c /dev/i2c-*
 chmod 660 /dev/i2c-*
@@ -26,10 +26,10 @@ chmod 660 /dev/i2c-*
 adduser tc i2c
 
 # remove running instances and start netmon async
-echo "Killing any old processes..."
+echo "---> Killing any old processes..."
 pgrep -f netmon.py && pkill -f netmon.py
-echo "Starting netmon..."
+echo "---> Starting netmon..."
 # run as user "tc"
 ( sudo -H -u tc $DIR/netmon.py &> /tmp/netmon.log ) &
 
-echo "Start sequence finished"
+echo "---> Start sequence finished"
