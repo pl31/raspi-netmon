bde676405ef70c323fdaaac5142a3698e9c5e28a
Patrick Büch <git@paco.pl31.de>
2016-06-11 13:53:54 +0200

    Use sudo -H

--- installer/installer.sh
+++ installer/installer.sh
@@ -4,10 +4,10 @@ echo "---> Running netmon installer"
 
 echo "---> Install required packages"
 tce-load -wi git iproute2 python python-setuptools py-smbus lighttpd tcpdump libcap-ng libnl
-sudo python -m ensurepip
-sudo pip install --upgrade pip
+sudo -H python -m ensurepip
+sudo -H pip install --upgrade pip
 echo "---> Install liquidcrystal_i2c"
-sudo pip install https://github.com/pl31/python-liquidcrystal_i2c/archive/master.zip
+sudo -H pip install https://github.com/pl31/python-liquidcrystal_i2c/archive/master.zip
 
 echo "---> Freshly clone repository to home folder"
 rm -rf /home/tc/raspi-netmon/
