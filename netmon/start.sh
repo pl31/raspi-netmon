450907a1b9b4706620697ba73f1da8f36c11da55
pl31 <patrick.kowalzick@web.de>
2018-03-15 21:08:00 +0100

    Disable web server and tcpdumps

--- netmon/start.sh
+++ netmon/start.sh
@@ -8,15 +8,15 @@ echo "---> Running $1"
 echo "---> Set eth0 to promiscious mode"
 ifconfig eth0 promisc
 
-echo "---> Starting lighttpd"
-rm -f /var/www/htdocs/index.html
-sed -i '/^dir-listing.activate/c\dir-listing.activate = "enable"' /usr/local/etc/lighttpd/conf.d/dirlisting.conf
-/usr/local/etc/init.d/lighttpd start
-# add symlinks for dumps (add pcap extension)
-for i in `seq 0 3`; do ln -s /tmp/tcpdumps/tcpdump_eth0_$i /var/www/htdocs/tcpdump_eth0_$i.pcap; done
-echo "---> Start tcpdump"
-mkdir -p /tmp/tcpdumps
-tcpdump -n -U -s 0 -i eth0 -W 4 -C 32M -w /tmp/tcpdumps/tcpdump_eth0_ "not ether host $(cat /sys/class/net/eth0/address)" &
+#echo "---> Starting lighttpd"
+#rm -f /var/www/htdocs/index.html
+#sed -i '/^dir-listing.activate/c\dir-listing.activate = "enable"' /usr/local/etc/lighttpd/conf.d/dirlisting.conf
+#/usr/local/etc/init.d/lighttpd start
+## add symlinks for dumps (add pcap extension)
+#for i in `seq 0 3`; do ln -s /tmp/tcpdumps/tcpdump_eth0_$i /var/www/htdocs/tcpdump_eth0_$i.pcap; done
+#echo "---> Start tcpdump"
+#mkdir -p /tmp/tcpdumps
+#tcpdump -n -U -s 0 -i eth0 -W 4 -C 32M -w /tmp/tcpdumps/tcpdump_eth0_ "not ether host $(cat /sys/class/net/eth0/address)" &
 
 echo "---> Create i2c group"
 addgroup i2c || echo "Error creating group i2c (already exists?)"
