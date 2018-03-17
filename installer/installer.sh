2f9eef996f6cf5a12a46c4f93cf3f109d0d90608
pl31 <patrick.kowalzick@web.de>
2018-03-17 22:18:25 +0100

    Install liquidcrystal_i2c globally

--- installer/installer.sh
+++ installer/installer.sh
@@ -6,7 +6,7 @@ echo "---> Install required packages"
 sudo apt install -y git python3-setuptools python3-setuptools-git python3-netifaces python3-smbus
 
 echo "---> Install missing modules"
-python3 -m easy_install --user git+https://github.com/pl31/python-liquidcrystal_i2c.git
+sudo python3 -m easy_install git+https://github.com/pl31/python-liquidcrystal_i2c.git
 
 echo "---> Freshly clone repository to home folder"
 rm -rf ~/raspi-netmon/
