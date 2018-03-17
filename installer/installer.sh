bb0eb024515b7014c45dd08c12578e1b3069a080
pl31 <patrick.kowalzick@web.de>
2018-03-17 22:12:52 +0100

    Refactoring run on RaspPi

--- installer/installer.sh
+++ installer/installer.sh
@@ -3,18 +3,22 @@
 echo "---> Running netmon installer"
 
 echo "---> Install required packages"
-apt install -y git python3-setuptools python3-setuptools-git python3-netifaces python3-smbus
+sudo apt install -y git python3-setuptools python3-setuptools-git python3-netifaces python3-smbus
 
 echo "---> Install missing modules"
 python3 -m easy_install --user git+https://github.com/pl31/python-liquidcrystal_i2c.git
 
 echo "---> Freshly clone repository to home folder"
-rm -rf /home/pi/raspi-netmon/
-git clone --depth=1 https://github.com/pl31/raspi-netmon.git /home/pi/raspi-netmon/
+rm -rf ~/raspi-netmon/
+git clone --depth=1 https://github.com/pl31/raspi-netmon.git ~/raspi-netmon/
 
-echo "---> Add start command to /opt/bootlocal.sh"
-BOOTLOCAL="/opt/bootlocal.sh"
-grep -q "# c0f40bf8" $BOOTLOCAL || echo '/home/tc/raspi-netmon/netmon/start.sh &> /tmp/start_netmon.log	# c0f40bf8' >> $BOOTLOCAL
+echo "---> Add netmon as systemd service"
+sudo cp ~/raspi-netmon/installer/netmon.service /etc/systemd/system
+sudo systemctl enable netmon.service
+
+echo "---> Set promiscuous mode fro eth0"
+sudo cp ~/raspi-netmon/installer/promiscuous@.service /etc/systemd/system
+sudo systemctl enable promiscuous@eth0.service
 
 echo
 echo "PLEASE REBOOT DEVICE"
